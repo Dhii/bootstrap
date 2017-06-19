6b13cb474dd5f61acf52e4ba1e0aef93a27fec22
Miguel Muscat <miguelmuscat93@gmail.com>
2017-06-19 14:49:45 +0200

    Build process uses intelligent guessing for vendor ...
    and package names via the author and project names.

--- build-functions.php
+++ build-functions.php
@@ -9,11 +9,48 @@
  *
  * @return string
  */
-function ucWordsKebabCase($kebab)
+function kebabCaseToUcWords($kebab)
 {
     $parts   = explode('-', $kebab);
     $ucParts = array_map('ucwords', $parts);
     $ucWords = implode(' ', $ucParts);
 
     return $ucWords;
+}
+
+/**
+ * Transforms a given string into kebab case.
+ *
+ * @since [*next-version*]
+ *
+ * @param string $string The string.
+ *
+ * @return string
+ */
+function toKebabCase($string)
+{
+    return implode('-', explode(' ', strtolower($string)));
+}
+
+function toCamelCase($string)
+{
+    $parts   = preg_split('/[-_\s]/', $string);
+    $ucWords = array_map('ucfirst', $parts);
+
+    return implode('', $ucWords);
+}
+
+/**
+ * Generates the namespace given the author and project names.
+ *
+ * @since [*next-version*]
+ *
+ * @param string $author   The author name.
+ * @param string $projName The project name.
+ *
+ * @return string
+ */
+function generateNamespace($author, $projName)
+{
+    return sprintf('%1$s\\%2$s', toCamelCase($author), toCamelCase($projName));
 }
