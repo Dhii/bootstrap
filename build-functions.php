82eaf51ee8bc5e8c425ea4bc6ea8e9c58690d330
Miguel Muscat <miguelmuscat93@gmail.com>
2017-06-19 13:47:03 +0200

    Build process now templates correct license file

--- build-functions.php
+++ build-functions.php
@@ -1,2 +1,19 @@
 <?php
 
+/**
+ * Transforms a kebab cased string into uppercased words.
+ *
+ * @since [*next-version*]
+ *
+ * @param string $kebab The kebab cased string.
+ *
+ * @return string
+ */
+function ucWordsKebabCase($kebab)
+{
+    $parts   = explode('-', $kebab);
+    $ucParts = array_map('ucwords', $parts);
+    $ucWords = implode(' ', $ucParts);
+
+    return $ucWords;
+}
